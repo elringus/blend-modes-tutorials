0d23be85fb9d2387948db9b23a7cf3f5d8ae9234
Artyom Sovetnikov <elringus@yandex.ru>
2018-07-05 15:12:10 +0300

    Initial commit

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,17 @@
+﻿// Copyright 2012-2018 Elringus (Artyom Sovetnikov). All Rights Reserved.
+
+using UnityEngine;
+
+namespace UnityCommon
+{
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField] private float speed = 30f;
+        [SerializeField] private Vector3 axis = new Vector3(1, 1, 1);
+
+        private void Update ()
+        {
+            transform.Rotate(axis, speed * Time.deltaTime);
+        }
+    }
+}
